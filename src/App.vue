eeae7375704d6b9fb8ffc8735259cf96f9199cb5
Devarsh Bhavsar <Devarsh.bhavsar01@gmail.com>
2020-12-30 21:49:22 -0500

    set up

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,69 @@
 <template>
-  <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
+    <v-app>
+        <v-app-bar
+          app
+          color="primary"
+          dark
+          dense
+        >
+            <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+            <v-toolbar-title>Devarsh</v-toolbar-title>
 
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
+            <v-spacer></v-spacer>
 
-      <v-spacer></v-spacer>
+            <v-btn
+                href="https://github.com/vuetifyjs/vuetify/releases/latest"
+                target="_blank"
+                text
+            >
+                <span class="mr-2">Latest Release</span>
+                <v-icon>mdi-open-in-new</v-icon>
+            </v-btn>
+        </v-app-bar>
+        <v-navigation-drawer
+            v-model="drawer"
+            absolute
+            temporary
+        >
+            <v-list
+                nav
+                dense
+            >
+                <v-list-item-group
+                    v-model="group"
+                    active-class="deep-purple--text text--accent-4"
+                >
+                    <v-list-item @click="goHome()">
+                        <v-list-item-icon>
+                            <v-icon>mdi-home</v-icon>
+                        </v-list-item-icon>
+                        <v-list-item-title>Home</v-list-item-title>
+                    </v-list-item>
+                </v-list-item-group>
+            </v-list>
+        </v-navigation-drawer>
+        <div style="height:48px"> </div>
+        <router-view></router-view>
+    </v-app>
 
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
-    </v-main>
-  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld
   },
 
   data: () => ({
-    //
-  })
+    drawer: false
+  }),
+
+  methods: {
+    goHome () {
+      this.$router.push({ name: 'Home' })
+    }
+  }
 }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-app>
+        <v-col cols="12" sm="6" md="3">
+          <v-text-field
+            label="Name"
+          ></v-text-field>
+        </v-col>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+
+  data: () => ({
+
+  })
+}
+</script>
